765bfd540219d5a13a85a421d11a0f3aa205dfa8
C4ptainN3mo <endra1632@gmail.com>
2022-08-24 16:23:50 +0700

    navbar for mobile

--- pages/components/navbar.jsx
+++ pages/components/navbar.jsx
@@ -1,15 +1,35 @@
 /* eslint-disable @next/next/no-img-element */
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
-
+import React from "react";
 export default function Navbar() {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
   return (
     <>
+      {/* Dekstop */}
       <div className="fixed w-full z-40">
         <div className="bg-blue-900 h-6"></div>
-        <div className="bg-blue-400 w-full h-20 bg-opacity-20 backdrop-blur-lg drop-shadow-lg flex items-center justify-between px-8 lg:px-16">
+        <div className="bg-blue-400 w-full h-20 bg-opacity-20 backdrop-blur-lg drop-shadow-lg flex items-center justify-between px-4 lg:px-16">
           <img src={"assets/banper.png"} alt="logo" className="h-36" />
-          <nav className="flex space-x-7 text-white">
+          <svg
+            onClick={() => setNavbarOpen(!navbarOpen)}
+            href="javascript:void(0);"
+            // onclick="openNav()"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 xl:hidden"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={"2"}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+          <nav className="xl:flex lg:flex md:hidden hidden space-x-7 text-black ">
             <Link href={"/home"}>
               <p className="hover:text-gray-900 text-sm outline-2 cursor-pointer">
                 Home
@@ -49,6 +69,83 @@ export default function Navbar() {
           </nav>
         </div>
       </div>
+      {/* Dekstop */}
+      {/* Mobile */}
+      <div
+        className={
+          "flex fixed flex-grow items-center w-full sm:hidden pt-28 bg-blue-400 bg-opacity-20 backdrop-blur-lg drop-shadow-lg pb-5" +
+          (navbarOpen ? " flex" : " hidden")
+        }
+        id="example-navbar-danger"
+      >
+        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+          <li className="nav-item">
+            <a
+              className="px-3 py-2 flex items-center text-xs   leading-snug text-black hover:opacity-75"
+              href="#home"
+            >
+              <i className="fab fa-facebook-square  leading-lg text-black opacity-75"></i>
+              <Link href={"/home"}>
+                <span className="ml-2">Home</span>
+              </Link>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="px-3 py-2 flex items-center text-xs   leading-snug text-black hover:opacity-75"
+              href="#skills"
+            >
+              <i className="fab fa-twitter  leading-lg text-black opacity-75"></i>
+              <span className="ml-2">Mekanisme Pendaftaran</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="px-3 py-2 flex items-center text-xs   leading-snug text-black hover:opacity-75"
+              href="#services"
+            >
+              <i className="fab fa-pinterest  leading-lg text-black opacity-75"></i>
+              <Link href={"/home"}>
+                <span className="ml-2">Berita</span>
+              </Link>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="px-3 py-2 flex items-center text-xs   leading-snug text-black hover:opacity-75"
+              href="#project"
+            >
+              <i className="fab fa-pinterest  leading-lg text-black opacity-75"></i>
+              <Link href={"/home"}>
+                <span className="ml-2">Galeri</span>
+              </Link>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="px-3 py-2 flex items-center text-xs   leading-snug text-black hover:opacity-75"
+              href="#project"
+            >
+              <div className="flex space-x-2 pl-2">
+                <Link href={"/auth/login"}>
+                  <span className="hover:text-gray-900  outline-2 cursor-pointer">
+                    Login
+                  </span>
+                </Link>
+                <span className="hover:text-gray-900  outline-2 cursor-pointer">
+                  |
+                </span>
+                <Link href={"/auth/login"}>
+                  <span className="hover:text-gray-900  outline-2 cursor-pointer">
+                    Daftar
+                  </span>
+                </Link>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* Mobile */}
     </>
   );
 }
