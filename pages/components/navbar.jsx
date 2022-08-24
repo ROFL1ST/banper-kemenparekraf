8d60d0d44d8ad07438ecbb4702e90ce9dd09a50e
unknown <you@example.com>
2022-08-24 17:35:52 +0700

    Banper

--- pages/components/navbar.jsx
+++ pages/components/navbar.jsx
@@ -2,6 +2,8 @@
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import React from "react";
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useRef, useState } from 'react'
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
 
@@ -36,10 +38,65 @@ export default function Navbar() {
               </p>
             </Link>
             <div className="cursor-pointer flex items-center space-x-1">
-              <p className="hover:text-gray-900 text-sm outline-2 cursor-pointer">
-                Mekanisme Pendaftaran
-              </p>
-              <ChevronDownIcon className="h-4 w-4" />
+              <Menu as="div" className="relative inline-block text-left">
+                <div>
+                  <Menu.Button className="inline-flex w-full justify-center   text-sm  hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                    Mekanisme Pendaftaran
+                    <ChevronDownIcon
+                      className="ml-2 -mr-1 h-5 w-5 "
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+                </div>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-sm bg-blue-900  bg-opacity-50 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className=" group flex w-full items-center rounded-md px-2 py-2 text-sm text-white"
+                          >
+
+                            F.A.Q
+                          </button>
+                        )}
+                      </Menu.Item>
+                      </div>
+                      <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className=" group flex w-full items-center rounded-md px-2 py-2 text-sm text-white"
+                          >
+
+                            Unduh Juknis
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                    <div className="px-1 py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className=" group flex w-full items-center rounded-md px-2 py-2 text-sm text-white"
+                          >
+
+                            Unduh Template
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </Menu.Items>
+                </Transition>
+              </Menu>
             </div>
             <Link href={"/home"}>
               <p className="hover:text-gray-900 text-sm outline-2 cursor-pointer">
