e07604e43d8e1fb47e155674c7715d86bc5936e5
C4ptainN3mo <endra1632@gmail.com>
2022-08-25 16:56:52 +0700

    Berita

--- pages/berita/card.jsx
+++ pages/berita/card.jsx
@@ -1,21 +1,40 @@
 /* eslint-disable @next/next/no-img-element */
-export default function Card() {
+const MAX_LENGTH = 95;
+
+export default function Card({ data }) {
+  const formatter = new Intl.DateTimeFormat("en-GB", {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  });
   return (
     <>
       <div className="bg-gray-100 w-full h-80 rounded-2xl">
         <div
-          className="w-full h-1/2 bg-cover rounded-t-2xl"
+          className="w-full h-1/2 bg-cover rounded-t-2xl bg-center"
           style={{
-            backgroundImage:
-              "url(https://c.inilah.com/2022/04/0419_010803_6f91_inilah.com_.jpg )",
+            backgroundImage: `url(${data.urlToImage})`,
           }}
         ></div>
         <div className="px-5 py-1">
-          <small className="text-xs font-bold text-gray-500">23 september 2022</small>
-          <h3 className="my-3 font-bold capitalize">
-            Pemerintah bakal revalitasi infrastruktur sektor ekonomi pasif
-          </h3>
-          <small className="text-xs font-bold text-blue-900">Kota Makassar</small>
+          <small className="text-xs font-bold text-gray-500">
+            {formatter.format(Date.parse(data.publishedAt))}
+          </small>
+          {data.title.length > MAX_LENGTH ? (
+            <h3 className="my-3 font-bold capitalize text-ellipsis">
+              {`${data.title.substring(0, MAX_LENGTH)}    ....`}
+              <a href="" className="text-blue-600">
+                Read more
+              </a>
+            </h3>
+          ) : (
+            <h3 className="my-3 font-bold capitalize text-ellipsis">
+              {data.title}
+            </h3>
+          )}
+          <small className="text-xs font-bold text-blue-900">
+            Kota Makassar
+          </small>
         </div>
       </div>
     </>
--- pages/berita/index.jsx
+++ pages/berita/index.jsx
@@ -1,30 +1,48 @@
 import Navbar from "../components/navbar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Menu from "./menu";
 import Card from "./card";
+import axios from "axios";
+import Footer from "../components/footer";
 
 export default function Berita() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const url =
+      "https://newsapi.org/v2/top-headlines?country=id&apiKey=7297761692414b5388644c4a35899ab3";
+    try {
+      let respond = await axios.get(url);
+      console.log(respond.data.articles);
+      setData(respond.data.articles);
+      setId(respond.data.articles.length);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    const ac = new AbortController();
+    getData();
+
+    return () => {
+      ac.abort();
+    };
+  }, []);
   useEffect(() => {
     document.title = "Berita";
   });
+  console.log(data);
   return (
     <>
       <Navbar />
       <Menu />
       <div className="pb-20 xl:px-20 px-10">
         <div className="grid xl:grid-cols-4 gap-3 mt-10">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {data.map((i, key) => (
+            <Card data={i} key={key} />
+          ))}
         </div>
       </div>
+      <Footer/>
     </>
   );
 }
