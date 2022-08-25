40909c3928e081f4d8a7c78f397b4205b8f369a7
C4ptainN3mo <endra1632@gmail.com>
2022-08-25 16:16:07 +0700

    Galery

--- pages/galeri/foto/foto.jsx
+++ pages/galeri/foto/foto.jsx
@@ -19,7 +19,7 @@ export default function Foto() {
             "url(https://cdn.pixabay.com/photo/2018/04/02/21/33/building-3285254_960_720.jpg)",
         }}
       >
-        <div className="bg-black h-full w-full bg-opacity-25 px-7 py-10">
+        <div className="bg-black h-full w-full bg-opacity-25 pl-7 2-xl:py-52 lg:py-32">
           <Swiper
             onSwiper={(swiper) => {
               swiperRef.current = swiper;
@@ -27,27 +27,29 @@ export default function Foto() {
             spaceBetween={20}
             slidesPerView={4}
           >
-            <SwiperSlide>
-              <h1 className="lg:text-6xl 2xl:text-8xl text-white font-semibold">Foto</h1>
-              <p className="text-white my-3 lg:text-sm 2xl:text-xl">
+            <SwiperSlide className="swiper-title">
+              <h1 className="lg:text-6xl 2xl:text-8xl text-white font-bold">
+                Foto
+              </h1>
+              <p className="text-white my-3 lg:text-sm 2xl:text-lg 2xl:w-3/4">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 necessitatibus? Quidem doloribus ex iure.
               </p>
-              <button className="bg-blue-900 bg-opacity-90 text-white px-5 lg:py-1 2xl:py-2 rounded-full 2xl:text-xl font-semibold">
+              <button className="bg-blue-900 bg-opacity-90 text-white px-5 lg:py-1 2xl:py-2 rounded-full 2xl:text-xl font-semibold mt-10">
                 Selengkapnya
               </button>
             </SwiperSlide>
             {imageLength.map((i, key) => (
-              <SwiperSlide key={key}>
+              <SwiperSlide className="swiper-image" key={key}>
                 <FotoCard
                   img={
-                    "https://asset.kompas.com/crops/FGkE00w3NfqPIrQTn_tTrrWyanA=/0x0:0x0/750x500/data/photo/2021/04/22/60819d99a708b.jpg"
+                    "https://awsimages.detik.net.id/visual/2021/12/30/cover-headline-sandiaga-uno-4_169.jpeg?w=360&q=90"
                   }
                 />
               </SwiperSlide>
             ))}
           </Swiper>
-          <div className="flex justify-center space-x-3 mt-5">
+          <div className="flex justify-center space-x-3 2xl:mt-10 mt-5">
             <button onClick={() => swiperRef.current.slidePrev()}>
               <ArrowLeftCircleIcon
                 className="lg:h-9 lg:w-9 2xl:h-12 2xl:w-12 text-white"
--- pages/galeri/video/video.jsx
+++ pages/galeri/video/video.jsx
@@ -12,13 +12,13 @@ export default function Video() {
   return (
     <>
       <div
-        className="w-full h-full bg-no-repeat bg-cover bg-bottom"
+        className="w-full h-full bg-no-repeat bg-cover bg-bottom rounded-b-3xl"
         style={{
           backgroundImage:
             "url(https://images.unsplash.com/photo-1513694203232-719a280e022f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80)",
         }}
       >
-        <div className="bg-black bg-opacity-50 h-full w-full py-5">
+        <div className="bg-black bg-opacity-50 h-full w-full 2xl:py-20 lg:py-16 py-5 rounded-b-3xl">
           <h1 className="text-6xl font-semibold text-white text-center">
             Video
           </h1>
@@ -82,7 +82,7 @@ export default function Video() {
               ></iframe>
             </SwiperSlide>
           </Swiper>
-          <div className="flex justify-center mt-5">
+          <div className="flex justify-center 2xl:mt-16 mt-5">
             <button className="bg-blue-500 text-white px-5 py-1 rounded-full">
               Selengkapnya
             </button>
