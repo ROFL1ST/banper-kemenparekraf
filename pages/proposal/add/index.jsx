a9f5252e5e2bda629020680551d936198260a9f4
C4ptainN3mo <endra1632@gmail.com>
2022-08-27 17:55:29 +0700

    Submit Proposal

--- pages/proposal/add/index.jsx
+++ pages/proposal/add/index.jsx
@@ -8,8 +8,14 @@ export default function Add() {
   return (
     <>
       <Navbar />
-      <div className="w-full h-screen">
-        <div className="w-full h-full lg:pt-32 lg:p-0 p-60 px-9  rounded-b-3xl">
+      <div
+        style={{
+          backgroundImage:
+            "url(https://cdn.pixabay.com/photo/2017/05/19/06/22/desk-2325627_960_720.jpg)",
+        }}
+        className="bg-gray-200 w-full h-screen bg-cover rounded-b-3xl"
+      >
+        <div className="bg-white w-full h-full bg-opacity-90 lg:pt-32 lg:p-0 p-60 px-9  rounded-b-3xl">
           <Section text={"Submit Proposal"} />
           <div className="mt-10 2xl:w-1/2 xl:w-3/5 lg:w-3/4 sm:w-3/5 2xl:space-y-10 lg:space-y-5 space-y-3  mx-auto">
             <div className="grid gap-y-2">
