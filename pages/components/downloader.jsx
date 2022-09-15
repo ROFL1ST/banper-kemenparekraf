82586dde861845ae6687f4585150051c1d2bd7de
C4ptainN3mo <endra1632@gmail.com>
2022-09-15 11:12:56 +0700

    P

--- pages/components/downloader.jsx
+++ pages/components/downloader.jsx
@@ -1,20 +1,32 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import useDownloader from "react-use-downloader";
+import { getDown } from "../api/restApi";
 function Downloader({ setOpen, setCheck }) {
-  const { size, elapsed, percentage, download, cancel, error, isInProgress } =
-    useDownloader();
   const { pathname } = useRouter();
-  const fileUrl = "http://128.199.242.242/dashboard/assets/juknisPetunjukTeknisBantuanPemerintahTahun2022.pdf";
-  const filename = "File.pdf";
+
+  const juknisUrl =
+    "http://128.199.242.242/dashboard/assets/juknisPetunjukTeknisBantuanPemerintahTahun2022.pdf";
+  const TemplateUrl =
+    "http://128.199.242.242/dashboard/assets/Dokumen_Banper_TA_2022.zip";
+
+  const getJuknis = async () => {
+    try {
+      let respond = await getDown(juknisUrl).then((result) => {
+        result;
+        console.log(result);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Link href={`${pathname === "/auth/login" ? "daftar" : "auth/daftar"}`}>
       <button
         onClick={() => {
           setOpen(false);
           setCheck(false);
-          download(fileUrl, filename);
+          getJuknis()
         }}
         type="submit"
         className="close mt-3 sm:mt-0 md:mt-0 lg:mt-0 22xl:mt-0 2xl:mt-0 w-full inline-flex justify-center rounded-[30px] border border-transparent shadow-sm px-7 lg:px-6 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:w-auto lg:text-sm"
