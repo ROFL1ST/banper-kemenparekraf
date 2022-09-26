aa0526a5c8df4f87c9c1bb53934ad61435edd98d
C4ptainN3mo <endra1632@gmail.com>
2022-09-26 09:18:35 +0700

    P

--- pages/components/navbar.jsx
+++ pages/components/navbar.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 import logo from "../assets/banper.png";
 import React from "react";
-import { Menu, Transition } from "@headlessui/react";
+import { Menu, Transition, Dialog } from "@headlessui/react";
 import { Fragment } from "react";
 import Router, { useRouter } from "next/router";
 
@@ -30,6 +30,9 @@ export default function Navbar({ open, setOpen }) {
     }
   }, [token]);
   // console.log(token);
+  const [log, setLog] = useState(false);
+  const cancelButtonRef = useRef(null);
+
   return (
     <>
       {/* Dekstop */}
@@ -306,6 +309,7 @@ export default function Navbar({ open, setOpen }) {
         )}
       </div>
       {/* Mobile */}
+      <LogOut log={log} setLog={setLog} cancelButtonRef={cancelButtonRef} />
     </>
   );
 }
@@ -549,6 +553,91 @@ function DropdownPeople({ setOpen, pathname }) {
           </Menu.Items>
         </Transition>
       </Menu>
+    </>
+  );
+}
+function LogOut({ log, setLog, cancelButtonRef }) {
+  return (
+    <>
+      <Transition.Root show={log} as={React.Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-50"
+          initialFocus={cancelButtonRef}
+          onClose={setLog}
+        >
+          <Transition.Child
+            as={React.Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+              <Transition.Child
+                as={React.Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="my-auto relative bg-white rounded-[30px] text-center overflow-hidden shadow-xl transform transition-all sm:my-8  xl:w-1/5 lg:w-1/4 md:w-2/5 sm:w-1/2 w-3/4  p-3">
+                  <div className="bg-white px-4 lg:px-1  pt-5 pb-4 ">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-center">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-lg md:text-sm leading-6 pb-3 font-medium text-gray-900"
+                        >
+                          Terms Of Service
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <p className="text-sm md:text-xs text-gray-500 justify-start text-start ">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Modi dignissimos dolor illum a recusandae
+                            soluta error architecto? Placeat architecto vel enim
+                            deleniti reprehenderit repudiandae, consequatur
+                            natus delectus odit sed, vero distinctio officiis
+                            necessitatibus. Corrupti, ut quo aperiam officia
+                            ullam enim corporis recusandae, ad culpa illum,
+                            tenetur maiores saepe consectetur exercitationem!
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="bg-gray-50 gap-x-10 px-4 py-5 lg:px-2 lg:py-3 sm:px-6 sm:flex justify-center ">
+                    <button
+                      type="button"
+                      className="close w-full inline-flex justify-center rounded-[30px] border border-gray-300 shadow-sm px-7 lg:px-6 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto lg:text-sm"
+                      onClick={() => setLog(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Decline
+                    </button>
+
+                    <button
+                      type="submit"
+                      className="close mt-3 sm:mt-0 md:mt-0 lg:mt-0 22xl:mt-0 2xl:mt-0 w-full inline-flex justify-center rounded-[30px] border border-transparent shadow-sm px-7 lg:px-6 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:w-auto lg:text-sm"
+                    >
+                      Log Out
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </>
   );
 }
