af5eead7c9a24a6c82c0eef1aaae1e7e92c23cd8
unknown <you@example.com>
2022-11-30 16:03:23 +0700

    banper

--- pages/components/feedback.jsx
+++ pages/components/feedback.jsx
@@ -5,17 +5,19 @@ import FeedbackIcon from '@mui/icons-material/Feedback';
 export default function Feedback() {
     return (
         <div>
-            <Fab size="large" color="primary" sx={{
-                height: 60,
-                width: 60,
+            <Fab variant="extended" size="large" color="primary" sx={{
+                height: 55,
+                width: 120,
                 position: 'fixed',
                 bottom: 16,
                 right: 16,
+                fontSize: 12,
+                fontWeight: 600,
             }}  onClick={() => {
                 window.open("mailto:banper@kemenparekraf.go.id")
             }} >
-                <FeedbackIcon sx={{height:35, width:35,}}  />
-                
+                <FeedbackIcon sx={{height:25, width:35,}}  />
+                Hubungi Kami
             </Fab>
         </div>
     )
