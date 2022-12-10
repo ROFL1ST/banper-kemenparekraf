f5cfb1b37e7beefde73be8df892324da7477a12a
root <setiawan.infor@gmail.com>
2022-12-10 13:19:51 +0000

    api

--- pages/api/restApi.js
+++ pages/api/restApi.js
@@ -1,6 +1,8 @@
 import * as axios from "axios";
-const BASE_URL = "http://128.199.242.242/api/";
-const DOWNLOAD_URL = "http://128.199.242.242/dashboard/";
+//const BASE_URL = "http://128.199.242.242/api/";
+//const DOWNLOAD_URL = "http://128.199.242.242/dashboard/";
+const BASE_URL = "https://banper.kemenparekraf.go.id/api/";
+const DOWNLOAD_URL = "https://banper.kemenparekraf.go.id/dashboard/";
 export function getPropose(path_url, token) {
   return new Promise((resolve, reject) => {
     var config = {
